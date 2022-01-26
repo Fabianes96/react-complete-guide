854cc6ccc51cdfb1c7aecd64713409c55b24c002
Fabian Higuita <fabianes1996@gmail.com>
2022-01-26 17:27:08 -0500

    Update ExpenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,21 +3,14 @@ import { Card } from '../UI/Card';
 import { ExpenseDate } from './ExpenseDate';
 import './ExpenseItem.css';
 
-const ExpenseItem = ({title,amount,date}) => {   
-    
-    const [titleName, setTitleName] = useState(title);
-    const clickHandler = ()=>{
-        setTitleName('Updated') 
-    }
-    
+const ExpenseItem = ({title,amount,date}) => {         
     return (
         <Card className='expense-item'>
             <ExpenseDate date={date}/>
             <div className='expense-item__description'>
-                <h2>{titleName}</h2>
+                <h2>{title}</h2>
             </div>
-            <div className='expense-item__price'>{amount}</div>
-            <button onClick={clickHandler}>Change title</button>            
+            <div className='expense-item__price'>{amount}</div>            
         </Card>
     );
 }
