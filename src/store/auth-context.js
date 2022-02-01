a1cc2308539b7fc7bf59542a2ceeb1806a790919
Fabian Higuita <fabianes1996@gmail.com>
2022-02-01 12:09:07 -0500

    Add AuthContextProvider

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogin: ()=>{},
+    onLogout: ()=>{}
 });
 
+export const AuthContextProvider = ({children})=>{
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+        if(storedUserLoggedInInformation === '1'){
+          setIsLoggedIn(true);
+        }  
+      }, []);
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const loginHandler = (email,password)=>{
+        localStorage.setItem("isLoggedIn", "1")
+        setIsLoggedIn(true)    }
+
+    const logoutHandler = ()=>{
+        localStorage.removeItem('isLoggedIn')
+        setIsLoggedIn(false)
+    }
+    return <AuthContext.Provider value={{isLoggedIn: isLoggedIn, onLogout: logoutHandler, onLogin: loginHandler}}>{children}</AuthContext.Provider>
+}
 export default AuthContext;
