e700ac1e35e429fa69550e0260e7902c11506d9a
Fabian Higuita <fabianes1996@gmail.com>
2022-02-01 16:29:07 -0500

    Update meals component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Header } from "./components/Header/Header";
+import { Meals } from "./components/Meals/Meals";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header title="ReactMeals" />
-    </div>
+      <main>
+        <Meals />
+      </main>
+    </>
   );
 }
 
