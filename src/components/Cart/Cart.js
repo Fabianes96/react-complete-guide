e15930ede82f01447416062d495073aca3e10f60
Fabian Higuita <fabianes1996@gmail.com>
2022-02-21 10:25:48 -0500

    Add CartItem component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,15 +2,29 @@ import React, { useContext } from "react";
 import { CartContext } from "../../store/cart-context";
 import { Modal } from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
-export const Cart = ({hideCart}) => {
-  const cartCtx = useContext(CartContext);  
+export const Cart = ({ hideCart }) => {
+  const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.amount.toFixed(2)}`;
   const hasItems = cartCtx.meals.length > 0;
+
+  const cartItemRemoveHandler = () => {};
+  const cartItemAddHandler = () => {};
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.meals.map((item) => {
-        return <li key={Math.random()}>{item.name}</li>;
+        return (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler}
+            onAdd={cartItemAddHandler}
+          />
+        );
       })}
     </ul>
   );
@@ -22,7 +36,9 @@ export const Cart = ({hideCart}) => {
         <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button onClick={hideCart} className={classes['button--alt']}>Close</button>
+        <button onClick={hideCart} className={classes["button--alt"]}>
+          Close
+        </button>
         {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
