01e13c558e92ccf99fecd3aa8d7ca1c5a792b8cc
Fabian Higuita <fabianes1996@gmail.com>
2022-02-21 10:43:31 -0500

    Update cart-context

--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -12,11 +12,25 @@ const initialCart ={
   totalAmount: 0,
 }
 
-const cartReducer =(state,action)=>{
+const cartReducer =(state,action)=>{  
   if(action.type==="ADD_ITEM"){   
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+    if(existingCartItem){      
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex]= updatedItem
+    }else{
+      updatedItems= [...state.items, action.item]
+    }
     return {
-      items: [...state.items,action.item],
-      totalAmount: state.totalAmount + action.item.price * action.item.amount
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
     }
   }
   return state;
