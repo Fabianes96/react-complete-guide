ab61045bed3ec43cecdd647ab2d16e5d54346493
Fabian Higuita <fabianes1996@gmail.com>
2022-02-23 10:48:24 -0500

    Add cart logic

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,17 +1,18 @@
 import classes from './CartButton.module.css';
 import {useDispatch} from 'react-redux';
 import { uiActions } from '../../store/UISlice';
+import { useSelector } from 'react-redux';
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
-
+  const quantity = useSelector(state=> state.cart.totalQuantity);
   const toggleCartHandler = ()=>{
     dispatch(uiActions.toggle());
   }
   return (
     <button onClick={toggleCartHandler} className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{quantity}</span>
     </button>
   );
 };
