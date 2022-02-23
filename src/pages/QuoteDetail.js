21466d2475c8f70b9684a367346d589cfc1a49df
Fabian Higuita <fabianes1996@gmail.com>
2022-02-23 18:30:13 -0500

    Add logic to show quotes by id

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Route, useParams } from 'react-router-dom'
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
+const DUMMY_QUOTES=[
+  {id: 'q1', author: 'I', text:'Learning React is fun!'},
+  {id: 'q2', author: 'Myself', text:'Learning React is great!'},
+]
 export const QuoteDetail = () => {
-  const params = useParams()
+  const params = useParams();
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+  if(!quote){
+    return <p>No quote find!</p>;
+  }
   return (
-    <section>
-      <div>QuoteDetail</div>
-      <p>{params.quoteId}</p>
+    <section>      
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
