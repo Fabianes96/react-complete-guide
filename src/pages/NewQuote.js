b1c6f11d4ab8591a8d2f1b3d9295e603d6f184e6
Fabian Higuita <fabianes1996@gmail.com>
2022-02-23 19:47:24 -0500

    Update Quote form & list

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import {useHistory} from 'react-router-dom'
 import QuoteForm from '../components/quotes/QuoteForm'
 
 
-export const NewQuote = quoteData => {
-  const addQuoteHandler = ()=>{
+export const NewQuote = () => {
+  const history = useHistory();
+  const addQuoteHandler = (quoteData)=>{
     console.log(quoteData);
+    history.push('/quotes')
   }
   return (
     <QuoteForm onAddQuote={addQuoteHandler}/>
